55077e396c9eddfe6fd55afedacaba5f837c7c28
Luis Ibanez <luis.ibanez@kitware.com>
2013-10-22 23:19:15 -0400

    Added C implementation of opp_dmode.

---
+++ sr_arm/opp_dmode.c
@@ -0,0 +1,20 @@
+/****************************************************************
+ *								*
+ *	Copyright 2001, 2009 Fidelity Information Services, Inc	*
+ *								*
+ *	This source code contains the intellectual property	*
+ *	of its copyright holder(s), and is made available	*
+ *	under a license.  If you do not know the terms of	*
+ *	the license, please stop and do not read further.	*
+ *								*
+ ****************************************************************/
+
+#include "mdef.h"
+
+
+void opp_dmode(void)
+{
+
+  op_dmode();
+
+}
